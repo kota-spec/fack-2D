91a33577d98b263fb7a33568bf2499db5f8d5446
市川功太 <daiya50.ezweb.ne.jp@gmail.com>
2019-12-23 22:50:26 +0900

    add: docsを追加

--- webpack.config.prod.babel.js
+++ webpack.config.prod.babel.js
@@ -1,8 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
 import merge from 'webpack-merge';
-import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
-import saveLicense from 'uglify-save-license';
 
 import baseConfig from './webpack.config.base.babel';
 
@@ -15,16 +13,6 @@ export default merge(baseConfig, {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new UglifyJsPlugin({
-      uglifyOptions: {
-        output: {
-          beautify: false,
-          comments: saveLicense
-        },
-        ie8: false,
-        warnings: false
-      }
     })
   ]
 });
